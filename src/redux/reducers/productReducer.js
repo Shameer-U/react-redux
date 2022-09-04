ad08a78869b6de314791ee613c51ecafee02630a
Shameer-U <shameerushahul@gmail.com>
2022-09-04 23:40:07 +0530

    comments about redux working

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -3,7 +3,13 @@ import { ActionTypes } from "../constants/action-types"
 const initialState = {
     products:[],
 }
-export const productReducer = (state = initialState, {type, payload}) => {
+
+//each time a function gets dispatched dispatch() function automatically passes corresponding "state" of each reducer function from combineReducers as first parameter.
+//whatever the reducer function returns, dispatch() function makes it as the new state of the corresponding reducer in combineReducers. 
+
+//so here each time this function gets dispatched the dispatch() function passes "allProducts" state from combineReducers (refer combineReducers in index.js) as first parameter to this function.
+//whatever this reducer function returns, dispatch() function makes it as the new "allProducts" state in combineReducers. 
+export const productReducer = (state = initialState, {type, payload}) => {  
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
             return { ...state, products: payload };
@@ -14,6 +20,9 @@ export const productReducer = (state = initialState, {type, payload}) => {
     }
 }
 
+
+//so here each time this function  gets dispatched the dispatch() function passes the current "product" state (refer combineReducers in index.js) as first parameter to this function.
+//whatever this reducer function returns, dispatch() function makes it as the new "product" state in combineReducers. 
 export const selectedProductReducer = (state = {}, {type, payload}) => {
     switch (type) {
         case ActionTypes.SELECTED_PRODUCT:
